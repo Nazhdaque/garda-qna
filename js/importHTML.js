94f44d1ee602c75e701194d5810b6a384a1cf5b8
nazhdaque <nazhdaque@email.com>
2025-09-08 08:24:59 +0300

    final sllide glow

--- js/importHTML.js
+++ js/importHTML.js
@@ -1,12 +1,12 @@
-import slide00 from "../html/slide-00.html?raw";
-import slide01 from "../html/slide-01.html?raw";
-import slide02 from "../html/slide-02.html?raw";
-import slide03 from "../html/slide-03.html?raw";
+import slide0 from "../html/slide-0.html?raw";
+import slide1 from "../html/slide-1.html?raw";
+import slide2 from "../html/slide-2.html?raw";
+import slide3 from "../html/slide-3.html?raw";
 import slideFinal from "../html/slide-final.html?raw";
 
 const mainContent = document.querySelector(".main-content");
-mainContent.insertAdjacentHTML("beforeend", slide00);
-mainContent.insertAdjacentHTML("beforeend", slide01);
-mainContent.insertAdjacentHTML("beforeend", slide02);
-mainContent.insertAdjacentHTML("beforeend", slide03);
+mainContent.insertAdjacentHTML("beforeend", slide0);
+mainContent.insertAdjacentHTML("beforeend", slide1);
+mainContent.insertAdjacentHTML("beforeend", slide2);
+mainContent.insertAdjacentHTML("beforeend", slide3);
 mainContent.insertAdjacentHTML("beforeend", slideFinal);
