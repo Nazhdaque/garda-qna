0f10ff9ebd0c61ec6c8a587b66b4257462964c19
nazhdaque <nazhdaque@gmail.com>
2025-09-13 09:20:43 +0300

    bubble

--- js/importHTML.js
+++ js/importHTML.js
@@ -2,6 +2,7 @@ import slide0 from "../html/slide-0.html?raw";
 import slide1 from "../html/slide-1.html?raw";
 import slide2 from "../html/slide-2.html?raw";
 import slide3 from "../html/slide-3.html?raw";
+import slide4 from "../html/slide-4.html?raw";
 import slideFinal from "../html/slide-final.html?raw";
 
 const mainContent = document.querySelector(".main-content");
@@ -9,4 +10,5 @@ mainContent.insertAdjacentHTML("beforeend", slide0);
 mainContent.insertAdjacentHTML("beforeend", slide1);
 mainContent.insertAdjacentHTML("beforeend", slide2);
 mainContent.insertAdjacentHTML("beforeend", slide3);
-mainContent.insertAdjacentHTML("beforeend", slideFinal);
+mainContent.insertAdjacentHTML("beforeend", slide4);
+// mainContent.insertAdjacentHTML("beforeend", slideFinal);
