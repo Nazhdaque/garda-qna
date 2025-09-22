1bd2676016240f02510382ebc07f9ca8b252b4ee
nazhdaque <nazhdaque@email.com>
2025-09-22 09:01:32 +0300

    img-maker

--- js/importHTML.js
+++ js/importHTML.js
@@ -3,6 +3,7 @@ import slide1 from "../html/slide-1.html?raw";
 import slide2 from "../html/slide-2.html?raw";
 import slide3 from "../html/slide-3.html?raw";
 import slide4 from "../html/slide-4.html?raw";
+import slide5 from "../html/slide-5.html?raw";
 import slideFinal from "../html/slide-final.html?raw";
 
 const mainContent = document.querySelector(".main-content");
@@ -11,4 +12,5 @@ mainContent.insertAdjacentHTML("beforeend", slide1);
 mainContent.insertAdjacentHTML("beforeend", slide2);
 mainContent.insertAdjacentHTML("beforeend", slide3);
 mainContent.insertAdjacentHTML("beforeend", slide4);
+mainContent.insertAdjacentHTML("beforeend", slide5);
 // mainContent.insertAdjacentHTML("beforeend", slideFinal);
